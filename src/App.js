48955daeddbf15b02dbee962ced6dbe526edf8bf
Garrett <garrettleegrahamanderson@gmail.com>
2023-09-28 18:55:03 -0400

    working on getting the hamburger menu to toggle on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import WeatherForceast from "./components/WeatherForcast";
+import MobileNav from "./components/Navigation/MobileNav";
+import Hamburger from "./components/Navigation/Hamburger";
 
 function App() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHambuger = () => {
+    console.log("hamburger open");
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Cottage</h1>
         <h2>Frazer-Garland Trust</h2>
       </header>
+
+      {/* Navigation menus */}
+      <Hamburger onClick={toggleHambuger} />
+      <MobileNav toggleNav={hamburgerOpen} />
       {/* Weather Component */}
       <WeatherForceast />
     </div>

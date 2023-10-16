27d2ab89186c280f777f6b5c395e3e9dfe7b6ff2
Garrett <garrettleegrahamanderson@gmail.com>
2023-10-16 17:06:29 -0400

    got the lists of cards for to dos, in progress and completed sections styled

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../assets/styles/Projects.css";
 
 export default function ProjectList() {
   return (
@@ -6,15 +7,87 @@ export default function ProjectList() {
       {/* To Do Projects */}
 
       <section className="to-do-projects">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">Project 1</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
+        <h1>To Do</h1>
+        <ol>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 1</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 2</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </section>
+      {/* Projects In Progress Section */}
+      <section className="in-progress-projects">
+        <h1>In Progress</h1>
+        <ol>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 1</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 2</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </section>
+      {/* Projects Completed Section */}
+      <section className="in-progress-projects">
+        <h1>Completed</h1>
+        <ol>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 1</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Project 2</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ol>
       </section>
     </section>
   );

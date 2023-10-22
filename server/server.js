4038c45903901fdb4760529777b79dfbe8c28bcf
Garrett <garrettleegrahamanderson@gmail.com>
2023-10-22 12:47:27 -0400

    created the middleware and connected to database. npm installed express and mysql

--- server/server.js
+++ server/server.js
@@ -3,3 +3,18 @@ const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "cottage_db",
+  },
+  console.log(`Connected to cottage_db database.`)
+);

5515f4dd298d558de5357240eff9b2c7980532bc
Garrett <garrettleegrahamanderson@gmail.com>
2023-10-22 15:20:28 -0400

    created seeds file to add projects to project table

---
+++ server/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO projects (project_details)
+VALUES ("Some quick example text to build on the card title and make up the bulk of the card's content."),
+       ("Some quick example text to build on the card title and make up the bulk of the card's content.")

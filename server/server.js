6b141f5a34d63a3f9efa09198986d2f7d47410ef
Garrett <garrettleegrahamanderson@gmail.com>
2023-10-22 13:20:40 -0400

    created query to post a project to the database, projects table

--- server/server.js
+++ server/server.js
@@ -18,3 +18,20 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to cottage_db database.`)
 );
+
+// Create a project
+app.post("/api/new-project", ({ body }, res) => {
+  const sql = `INSERT INTO projects (project_details) VALUES (?)`;
+  const params = [body.project_details];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});

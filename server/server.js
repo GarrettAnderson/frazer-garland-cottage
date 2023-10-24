7c1b65ef8b5eaec030e2b0049f7fc4ea79898fb0
Garrett <garrettleegrahamanderson@gmail.com>
2023-10-24 17:38:14 -0400

    working on getting the delete endpoint to work

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", true);
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });

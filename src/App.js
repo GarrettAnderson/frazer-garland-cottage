026e9928d3939a2f9696a135c9e2045d8ac2e378
Garrett <garrettleegrahamanderson@gmail.com>
2025-03-07 16:45:26 -0500

    recreated the _redirects file with the /* /index.html 200 suggested by chatgpt for refresh issues. also replaced BrowserRouter in App.js because it was breaking localhost rendering - the same thing i was seeing on gh-pages site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import {
-  HashRouter as /* BrowserRouter */ Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   HashRouter as /* BrowserRouter */ Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Availability from "./components/Availability";
@@ -13,16 +14,16 @@ import ProjectList from "./components/ProjectList";
 function App() {
   return (
     <div className="App-container">
-      {/* <BrowserRouter> */}
-      <Router basename="/frazer-garland-cottage">
+      <BrowserRouter>
+        {/* <Router basename="/frazer-garland-cottage"> */}
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/availability" element={<Availability />}></Route>
           <Route exact path="/records" element={<ManagementRecord />}></Route>
           <Route exact path="/projects" element={<ProjectList />}></Route>
         </Routes>
-      </Router>
-      {/* </BrowserRouter> */}
+        {/* </Router> */}
+      </BrowserRouter>
     </div>
   );
 }

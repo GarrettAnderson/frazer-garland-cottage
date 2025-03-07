dadbc98907f1e4fa748278662228069559aec0af
Garrett <garrettleegrahamanderson@gmail.com>
2025-03-07 16:55:31 -0500

    commented out the BrowserRouter and replaced with HashRouter so the site will work on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
-// import {
-//   HashRouter as /* BrowserRouter */ Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Availability from "./components/Availability";
@@ -14,16 +10,16 @@ import ProjectList from "./components/ProjectList";
 function App() {
   return (
     <div className="App-container">
-      <BrowserRouter>
-        {/* <Router basename="/frazer-garland-cottage"> */}
+      {/* <BrowserRouter>s */}
+      <Router basename="/frazer-garland-cottage">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/availability" element={<Availability />}></Route>
           <Route exact path="/records" element={<ManagementRecord />}></Route>
           <Route exact path="/projects" element={<ProjectList />}></Route>
         </Routes>
-        {/* </Router> */}
-      </BrowserRouter>
+      </Router>
+      {/* </BrowserRouter> */}
     </div>
   );
 }

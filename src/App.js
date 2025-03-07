32f06d4c69e5b27fcec5bbebae66ac6f4284ddbf
Garrett <garrettleegrahamanderson@gmail.com>
2025-03-07 15:56:05 -0500

    replaced BrowserRouter with HashRouter from react-router-dom because BrowserRouter is server side and therefore not supported by gh-pages which is most likely causing the rendering errors on the gh-pages site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as /* BrowserRouter */ Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Availability from "./components/Availability";
@@ -9,14 +13,16 @@ import ProjectList from "./components/ProjectList";
 function App() {
   return (
     <div className="App-container">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/availability" element={<Availability />}></Route>
           <Route exact path="/records" element={<ManagementRecord />}></Route>
           <Route exact path="/projects" element={<ProjectList />}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
+      {/* </BrowserRouter> */}
     </div>
   );
 }
